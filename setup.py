f2ab0aff560c582c4b5b0be34232d3758dbdfccd
rafalmucha <6rafel6@gmail.com>
2019-05-16 13:34:10 +0200

    add new structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Description here',
+    author='Leszek Grzanka, Jakub Luśtyk, Kacper Janda, Karol Bąk, Piotr Ćwiklicki, Rafał Mucha',
+    license='MIT',
+)
