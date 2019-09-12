42809d0672d08b03bd710c9c31914ed39775ea46
Rafal Mucha <6rafel6@gmail.com>
2019-09-12 15:50:19 +0200

    Another refactor

--- src/ptc_track/optics_parameters.py
+++ src/ptc_track/optics_parameters.py
@@ -6,79 +6,50 @@ import utils.differential_quotient as optical_function_as_differential
 
 
 def compute_v_x(madx_configuration, grid_configuration):
-    particles = pg.generate_from_range(grid_configuration)
-
-    transporter = apply_configuration_to_transporter(madx_configuration)
-    normalized_transporter = normalize_ptc_track_transporter_output(transporter)
-
-    particles_with_optical_function = optical_function_as_differential.compute_optical_function(normalized_transporter,
-                                                                                                particles,
-                                                                                                "x", "x", 1e-5)
+    particles_with_optical_function = compute_optical_function(madx_configuration, grid_configuration, "x", "x", 1e-5)
 
     return particles_with_optical_function
 
 
 def compute_v_y(madx_configuration, grid_configuration):
-    particles = pg.generate_from_range(grid_configuration)
-
-    transporter = apply_configuration_to_transporter(madx_configuration)
-    normalized_transporter = normalize_ptc_track_transporter_output(transporter)
-
-    particles_with_optical_function = optical_function_as_differential.compute_optical_function(normalized_transporter,
-                                                                                                particles,
-                                                                                                "y", "y", 1e-5)
+    particles_with_optical_function = compute_optical_function(madx_configuration, grid_configuration, "y", "y", 1e-5)
 
     return particles_with_optical_function
 
 
 def compute_l_x(madx_configuration, grid_configuration):
-    particles = pg.generate_from_range(grid_configuration)
-
-    transporter = apply_configuration_to_transporter(madx_configuration)
-    normalized_transporter = normalize_ptc_track_transporter_output(transporter)
-
-    particles_with_optical_function = optical_function_as_differential.compute_optical_function(normalized_transporter,
-                                                                                                particles,
-                                                                                                "theta x", "x", 1e-5)
+    particles_with_optical_function = compute_optical_function(madx_configuration, grid_configuration, "theta x", "x", 1e-5)
 
     return particles_with_optical_function
 
 
 def compute_l_y(madx_configuration, grid_configuration):
-    particles = pg.generate_from_range(grid_configuration)
-
-    transporter = apply_configuration_to_transporter(madx_configuration)
-    normalized_transporter = normalize_ptc_track_transporter_output(transporter)
-
-    particles_with_optical_function = optical_function_as_differential.compute_optical_function(normalized_transporter,
-                                                                                                particles,
-                                                                                                "theta y", "y", 1e-5)
+    particles_with_optical_function = compute_optical_function(madx_configuration, grid_configuration, "theta y", "y", 1e-5)
 
     return particles_with_optical_function
 
 
 def compute_d_x(madx_configuration, grid_configuration):
-    particles = pg.generate_from_range(grid_configuration)
-
-    transporter = apply_configuration_to_transporter(madx_configuration)
-    normalized_transporter = normalize_ptc_track_transporter_output(transporter)
-
-    particles_with_optical_function = optical_function_as_differential.compute_optical_function(normalized_transporter,
-                                                                                                particles,
-                                                                                                "pt", "x", 1e-5)
+    particles_with_optical_function = compute_optical_function(madx_configuration, grid_configuration, "pt", "x", 1e-5)
 
     return particles_with_optical_function
 
 
 def compute_d_y(madx_configuration, grid_configuration):
-    particles = pg.generate_from_range(grid_configuration)
+    particles_with_optical_function = compute_optical_function(madx_configuration, grid_configuration, "pt", "y", 1e-5)
+    return particles_with_optical_function
 
+
+def compute_optical_function(madx_configuration, grid_configuration, delta_parameter_name, transported_parameter_name, 
+                             delta_alternative_value):
+    particles = pg.generate_from_range(grid_configuration)
+    particles_in_geometrical_coordinates = transform_to_geometrical_coordinates(particles)
     transporter = apply_configuration_to_transporter(madx_configuration)
     normalized_transporter = normalize_ptc_track_transporter_output(transporter)
 
-    particles_with_optical_function = optical_function_as_differential.compute_optical_function(normalized_transporter,
-                                                                                                particles,
-                                                                                                "pt", "y", 1e-5)
+    particles_with_optical_function = optical_function_as_differential.\
+        compute_optical_function(normalized_transporter, particles_in_geometrical_coordinates, 
+                                 delta_parameter_name, transported_parameter_name, delta_alternative_value)
 
     return particles_with_optical_function
 
