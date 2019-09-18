39f29e760870585848219eed04f675e3d9963233
Rafal Mucha <6rafel6@gmail.com>
2019-09-18 13:48:13 +0200

    Update grid configuration

--- src/data/bunch_configuration.py
+++ src/data/bunch_configuration.py
@@ -1,39 +1,43 @@
-class BunchConfiguration:
+class GridConfiguration:
     def __init__(self,
-                 x_min, x_max, number_of_x_values,
-                 theta_x_min, theta_x_max, number_of_theta_x_values,
-                 y_min, y_max, number_of_y_values,
-                 theta_y_min, theta_y_max, number_of_theta_y_values,
-                 pt_min, pt_max, number_of_pt_values):
+                 x_min, x_max, x_resolution,
+                 theta_x_min, theta_x_max, theta_x_resolution,
+                 y_min, y_max, y_resolution,
+                 theta_y_min, theta_y_max, theta_y_resolution,
+                 xa_min, xa_max, xa_resolution,
+                 pt_min, pt_max, pt_resolution):
         self.x_min = x_min
         self.x_max = x_max
-        self.number_of_x_values = number_of_x_values
+        self.x_resolution = x_resolution
         self.y_min = y_min
         self.y_max = y_max
-        self.number_of_y_values = number_of_y_values
+        self.y_resolution = y_resolution
         self.theta_x_min = theta_x_min
         self.theta_x_max = theta_x_max
-        self.number_of_theta_x_values = number_of_theta_x_values
+        self.theta_x_resolution = theta_x_resolution
         self.theta_y_min = theta_y_min
         self.theta_y_max = theta_y_max
-        self.number_of_theta_y_values = number_of_theta_y_values
+        self.theta_y_resolution = theta_y_resolution
+        self.xa_min = xa_min
+        self.xa_max = xa_max
+        self.xa_resolution = xa_resolution
         self.pt_min = pt_min
         self.pt_max = pt_max
-        self.number_of_pt_values = number_of_pt_values
+        self.pt_resolution = pt_resolution
 
     @staticmethod
-    def from_sigma(x_mean, sigma_x, number_of_x_values,
-                   theta_x_mean, sigma_theta_x, number_of_theta_x_values,
-                   y_mean, sigma_y, number_of_y_values,
-                   theta_y_mean, sigma_theta_y, number_of_theta_y_values,
-                   pt_mean, sigma_pt, number_of_pt_values):
-        return BunchConfiguration(x_mean - sigma_x, x_mean + sigma_x, number_of_x_values,
-                                  theta_x_mean - sigma_theta_x, theta_x_mean + sigma_theta_x, number_of_theta_x_values,
-                                  y_mean - sigma_y, y_mean + sigma_y, number_of_y_values,
-                                  theta_y_mean - sigma_theta_y, theta_y_mean + sigma_theta_y, number_of_theta_y_values,
-                                  pt_mean - sigma_pt, pt_mean + sigma_pt, number_of_pt_values)
+    def from_sigma(x_mean, sigma_x, x_resolution,
+                   theta_x_mean, sigma_theta_x, theta_x_resolution,
+                   y_mean, sigma_y, y_resolution,
+                   theta_y_mean, sigma_theta_y, theta_y_resolution,
+                   pt_mean, sigma_pt, pt_resolution):
+        return GridConfiguration(x_mean - sigma_x, x_mean + sigma_x, x_resolution,
+                                 theta_x_mean - sigma_theta_x, theta_x_mean + sigma_theta_x, theta_x_resolution,
+                                 y_mean - sigma_y, y_mean + sigma_y, y_resolution,
+                                 theta_y_mean - sigma_theta_y, theta_y_mean + sigma_theta_y, theta_y_resolution,
+                                 pt_mean - sigma_pt, pt_mean + sigma_pt, pt_resolution)
 
     def get_number_of_particles(self):
-        number_of_particles = self.number_of_pt_values * self.number_of_theta_y_values * self.number_of_y_values * \
-            self.number_of_x_values * self.number_of_theta_x_values
+        number_of_particles = self.pt_resolution * self.theta_y_resolution * self.y_resolution * \
+            self.x_resolution * self.theta_x_resolution * self.xa_resolution
         return number_of_particles
