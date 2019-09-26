c5f2e6484b567cf2bec5449cd908c1d4231e99a1
Rafal Mucha <6rafel6@gmail.com>
2019-09-26 09:02:18 +0200

    Adjust extrapolation transport to use Particles object

--- src/extrapolated_crossing_angle/transporter.py
+++ src/extrapolated_crossing_angle/transporter.py
@@ -1,18 +1,22 @@
 import ptc_track.transporter as ptg
+from data.particles import Particles
 
 
 def transport(particles, wanted_xa, configuration_madx_xa1, xa1,
               configuration_madx_xa2, xa2):
-    particles_xa_1 = ptg.transport(configuration_madx_xa1, particles)
-    particles_xa_2 = ptg.transport(configuration_madx_xa2, particles)
+    segments_xa_1 = ptg.transport(configuration_madx_xa1, particles)
+    segments_xa_2 = ptg.transport(configuration_madx_xa2, particles)
 
     new_places = {}
-    for place in particles_xa_1.keys():
+    for place in segments_xa_1.keys():
         # (X1 - X2) / (XA1 - XA2) * XA + X2
-        ax = (particles_xa_1[place] - particles_xa_2[place]) / (xa1 - xa2) * (wanted_xa - xa2)
-        b = particles_xa_2[place]
+        # todo refactor, it break isolation of Particles object
+        particles_xa_1 = segments_xa_1[place].get_matrix()
+        particles_xa_2 = segments_xa_2[place].get_matrix()
+        ax = (particles_xa_1 - particles_xa_2) / (xa1 - xa2) * (wanted_xa - xa2)
+        b = particles_xa_2
         particles_with_xa = ax + b
-        new_places[place] = particles_with_xa
+        new_places[place] = Particles(particles_with_xa, particles_xa_1.get_mapping())
 
     return new_places
 
