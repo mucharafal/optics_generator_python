488abb96ddca4c28ed8de26b9cdef261f23b1326
Rafal Mucha <6rafel6@gmail.com>
2019-09-26 10:02:40 +0200

    Adjust twiss to use Particles object

--- src/twiss/matrix_indexes.py
+++ src/twiss/matrix_indexes.py
@@ -5,7 +5,7 @@ column=s,x,y,px,py,betx,bety,mu1,mu2,re12,re34,disp1,disp3;
 If above line in madx script is changed, adjust below map.
 Last column (pt) is appended after read in from file and is always last column.
 """
-ptc_twiss = {
+twiss = {
     Parameters.S: 0,
     Parameters.X: 1,
     Parameters.Y: 2,
--- src/twiss/transporter.py
+++ src/twiss/transporter.py
@@ -1,7 +1,17 @@
 import twiss.runner as runner
+from data.particles import Particles
+from data.parameters_names import ParametersNames as Parameters
+import twiss.matrix_indexes
 
 
 def get_transporter(madx_configuration):
     def transporter(particles):
-        return runner.transport(madx_configuration, particles)
+        raw_particles = particles.get_coordinates(Parameters.X, Parameters.THETA_X, Parameters.Y, Parameters.THETA_Y,
+                                                  Parameters.PT)
+        result_particles_object = Particles(runner.transport(madx_configuration, raw_particles), get_mapping())
+        return result_particles_object
     return transporter
+
+
+def get_mapping():
+    return twiss.matrix_indexes.twiss
