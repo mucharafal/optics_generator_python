94da0267426eebaaf7455d944fafe7869e21de94
Rafal Mucha <6rafel6@gmail.com>
2019-11-15 15:20:30 +0100

    Update commands to work on SWAN

--- commands.sh
+++ commands.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ "$1" = "--make_parameterization" ] ; then
-    ./src/models/make_parameterization.py $2 $3
+    python3 ./src/models/make_parameterization.py $2 $3
 elif [ "$1" = "--make_error_plots" ] ; then
     python3 ./src/output_generation/plots_errors.py $2 $3 $4 $5
 elif [ "$1" = "--make_optical_functions_plot" ] ; then
