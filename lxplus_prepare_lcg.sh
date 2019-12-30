79b5aa6902d0edf422c50ccb87a74bdbee638ce7
Rafal Mucha <6rafel6@gmail.com>
2019-12-30 16:25:33 +0100

    Add to start script lxplus fragment and shebang to lxplus_prepare_script

--- lxplus_prepare_lcg.sh
+++ lxplus_prepare_lcg.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Information about software stack avaliable on lxplus
 # http://lcginfo.cern.ch/
 # Commands semantics:
@@ -9,7 +10,7 @@
 
 export LCGENV_PATH=/cvmfs/sft.cern.ch/lcg/releases
 lcgenv='/cvmfs/sft.cern.ch/lcg/releases/lcgenv/latest/lcgenv'
-$lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt cpymad >> lcg.sh
+$lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt cpymad > lcg.sh
 $lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt jupyter >> lcg.sh
 $lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt seaborn >> lcg.sh
 $lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt ROOT >> lcg.sh
