c189681ca2b723712c89971b6e7e2875f66cd695
Rafal Mucha <6rafel6@gmail.com>
2019-12-30 14:33:51 +0100

    Add export of configuration of lcg on lxplus

---
+++ lxplus_prepare_lcg.sh
@@ -0,0 +1,15 @@
+# Information about software stack avaliable on lxplus
+# http://lcginfo.cern.ch/
+# Commands semantics:
+# lcgenv stack_name platform package_name
+# returns "export" commands which should be invoke to include package
+# all of them are redirected to lcg.sh file, which should be executed before using
+# of tools
+# Before using lcgenv variable LCGENV_PATH should be set properly
+
+export LCGENV_PATH=/cvmfs/sft.cern.ch/lcg/releases
+alias lcgenv='/cvmfs/sft.cern.ch/lcg/releases/lcgenv/latest/lcgenv'
+lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt cpymad >> lcg.sh
+lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt jupyter >> lcg.sh
+lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt seaborn >> lcg.sh
+lcgenv -p LCG_96python3 x86_64-centos7-gcc8-opt ROOT >> lcg.sh
